f667c2695b831e80f4a339216c27f0e658975bf5
Nader Nasr <nadernasr7@gmail.com>
2018-05-22 12:02:27 +0300

    Addition to AR Scripts

---
+++ app/AR_Scenes/Shark.js
@@ -0,0 +1,77 @@
+'use strict';
+
+import React, { Component } from 'react';
+
+import {StyleSheet} from 'react-native';
+
+import {
+  ViroSceneNavigator,
+  ViroScene,
+  ViroARScene,
+  ViroAmbientLight,
+  Viro360Video,
+  Viro360Image,
+  ViroUtils,
+  ViroPortal,
+  ViroPortalScene,
+  Viro3DObject,
+  ViroARPlaneSelector,
+} from 'react-viro';
+
+export default class HelloWorldSceneAR extends Component {
+  constructor() {
+    super();
+
+    // Set initial state here
+    this.state = {
+      text : "initializing AR..."
+    };
+
+    // bind 'this' to functions
+    this._onInitialized = this._onInitialized.bind(this);
+  }
+
+  render() {
+    return (
+    <ViroARScene>
+      <ViroARPlaneSelector />
+        <ViroAmbientLight color="#ffffff" intensity={200}/>
+          <ViroPortalScene passable={true} dragType="FixedToWorld" >
+            <ViroPortal position={[0, 0, -1]} scale={[0.1, 0.1, 0.1]}>
+
+              <Viro3DObject source={require('./portal_res/Shark/Shark.obj')}
+                resources={[require('./portal_res/Shark/greatwhiteanatomy.jpg'),
+                            require('./portal_res/Shark/SharlAnatomy3.jpg'),
+                            require('./portal_res/Shark/GreatWhiteDiffuse.png')]}
+                    type="OBJ"/>
+
+            </ViroPortal>
+              </ViroPortalScene>
+            </ViroARScene>
+    );
+  }
+
+  _onInitialized(state, reason) {
+    if (state == ViroConstants.TRACKING_NORMAL) {
+      this.setState({
+        text : "HEY RBK!"
+      });
+    } else if (state == ViroConstants.TRACKING_NONE) {
+      // Handle loss of tracking
+    }
+  }
+}
+
+var styles = StyleSheet.create({
+  helloWorldTextStyle: {
+    fontFamily: 'Arial',
+    fontSize: 30,
+    color: 'black',
+    textAlignVertical: 'center',
+    textAlign: 'center',
+  },
+});
+
+
+
+module.exports = HelloWorldSceneAR;
